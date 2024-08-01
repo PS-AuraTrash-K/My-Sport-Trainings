170b75814b89782cc7f8f11f2fd6f55d51ae767d
PS-AuraTrash-K <taras.shparuk@gmail.com>
2024-08-01 14:21:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Kotlin project My Sport Trainings
---
+++ app/src/main/java/com/jimbosht/mysporttrainings/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.jimbosht.mysporttrainings
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jimbosht.mysporttrainings.ui.theme.MySportTrainingsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreetingPreview()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MySportTrainingsTheme {
+        Greeting("Android")
+    }
+}
